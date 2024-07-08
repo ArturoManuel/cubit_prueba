3a805f3f3b0696b47d7cb43a6239865eae49afae
ArturoManuel <noriegaarturonoriega@gmail.com>
2024-07-07 20:46:04 -0500

    .

--- lib/presentation/dropdown/dropdown.dart
+++ lib/presentation/dropdown/dropdown.dart
@@ -1,13 +1,5 @@
-
 import 'package:cubic_prueba/presentation/dropdown/provider.dart';
 import 'package:cubic_prueba/presentation/dropdown/widget/customDropdown.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 class DropdownMenuExample extends StatelessWidget {
@@ -24,8 +16,7 @@ class DropdownMenuExample extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CustomDropdown(),
-            if (dropdownProvider.displayedCountry != null &&
-                dropdownProvider.displayedState != null)
+            if (dropdownProvider.displayedCountry != null && dropdownProvider.displayedState != null)
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Text(
